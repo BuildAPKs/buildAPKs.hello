0ff8dc876c1a8671c743269079ff99387f5cbb8e
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-06-30 06:42:06 -0400

    commit 477294577 on 20210630

--- ma.bash
+++ ma.bash
@@ -1,13 +1,14 @@
-#!/bin/env bash # Copyright 2019 (c) all rights reserved 
-# by BuildAPKs https://buildapks.github.io
-# File ~/RDR/conf/README.md has more information about this file.
+#!/usr/bin/env bash # Copyright 2019-2020 (c) all rights reserved 
+# by BuildAPKs https://buildapks.github.io see LICENSE
+# File RDR/conf/README.md has more information about this file.
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
-. "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212
+. "$RDR/scripts/bash/shlibs/trap.bash" 210 211 212
 cd "$JDR"
 # _AT_ username/repository commit
+_AT_ DavidYKay/HelloMaven a975b4caed549a655ee49be7e1e00befced41031
 _AT_ han1202012/ApplicationDemo 0073a0292bba1c963c94f036f7b396707d6c52ec
 _AT_ han1202012/DisplayTest 32c14355589367aae65916315c7f4f1d170de011
 _AT_ ti3r/alert-dialog-with-options-list cdf89cc4f54a70b87a1b2e6086a454c75a5864f7
-# ma.bash OEF
+# BuildAPKs/buildAPKs.hellos ma.bash OEF
