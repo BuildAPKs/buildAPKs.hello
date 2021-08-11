96779b759964770ded7ff158cdf7236ce21db97c
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-08-11 10:31:35 -0400

    commit 387272198 on 20210811

--- ma.bash
+++ ma.bash
@@ -12,5 +12,4 @@ _AT_ han1202012/ApplicationDemo 0073a0292bba1c963c94f036f7b396707d6c52ec
 _AT_ han1202012/DisplayTest 32c14355589367aae65916315c7f4f1d170de011
 _AT_ ti3r/alert-dialog-with-options-list cdf89cc4f54a70b87a1b2e6086a454c75a5864f7
 _AT_ toptierlabs/AndroidMp3Player cb58dd8172d18665088b3bb2be83cd65bd735de1
-_AT_ vitaly-little/RRatoraktInternetRadio 0789ed433d104aece88c1ecacf21ea603d3fd9be
 # BuildAPKs/buildAPKs.hellos ma.bash OEF
